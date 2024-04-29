88067efdb97a953ebadd285b5a52461f8ca07735
merefield <merefield@gmail.com>
2024-04-29 08:52:03 +0100

    Add alternative routes for dynamic pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 DiscourseHomePages::Engine.routes.draw do
-  get 'page/:page_id' => 'page#index'
+  get 'landing-page/:page_id' => 'page#index'
+  get 'home-page/:page_id' => 'page#index'
 end
 
 Discourse::Application.routes.append do
-  pp "mounting DiscourseHomePages::Engine at discourse-home-pages"
   mount ::DiscourseHomePages::Engine, at: 'discourse-home-pages'
 end
